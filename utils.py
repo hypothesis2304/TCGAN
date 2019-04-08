781736db38e9d5a251c462911abf0146990ed20d
Bhadrinath Nagabandi (Student) <bnagaban@asu.edu>
2019-04-07 21:12:09 -0700

    util functions

---
+++ utils.py
@@ -0,0 +1,15 @@
+import random
+import time
+import datetime
+import sys
+
+from torch.autograd import Variable
+import torch
+from visdom import Visdom
+import numpy as np
+
+def tensor2image(tensor):
+    image = 127.5*(tensor[0].cpu().float().numpy() + 1.0)
+    if image.shape[0] == 1:
+        image = np.tile(image, (3,1,1))
+    return image.astype(np.uint8)
