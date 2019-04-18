74a015c29b3a63cadf92326352615436ba0e27f8
Bhadrinath Nagabandi (Student) <bnagaban@asu.edu>
2019-04-18 16:07:00 -0700

    solved errors, fixed netG input and reconstructions, identity outputs

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import argparse
 import itertools
 import torchvision.transforms as transforms
-import tqdm
+from tqdm import tqdm, trange
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
 from PIL import Image
@@ -18,24 +18,28 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--epoch', type=int, default=0, help='starting epoch')
 parser.add_argument('--n_epochs', type=int, default=200, help='number of epochs of training')
 parser.add_argument('--batchSize', type=int, default=64, help='size of the batches')
-parser.add_argument('--dataroot', type=str, default='datasets/horse2zebra/', help='root directory of the dataset')
+parser.add_argument('--dataroot', type=str, default="./datasets/facades/", help='root directory of the dataset')
 parser.add_argument('--lr', type=float, default=0.0002, help='initial learning rate')
 parser.add_argument('--decay_epoch', type=int, default=100, help='epoch to start linearly decaying the learning rate to 0')
 parser.add_argument('--size', type=int, default=256, help='size of the data crop (squared assumed)')
-parser.add_argument('--input_nc', type=int, default=5, help='number of channels of input data')
+parser.add_argument('--input_nc', type=int, default=3, help='number of channels of input data')
 parser.add_argument('--output_nc', type=int, default=3, help='number of channels of output data')
 args = parser.parse_args()
 print(args)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 data_path = "./datasets/facades/"
+save_dir = "./datasets/facades/" + '_results/'
+model_dir = "./datasets/facades/" + '_model/'
+
 batch_size = args.batchSize
 n_epochs = args.n_epochs
 
 if torch.cuda.is_available():
     print("GPU is available!")
 
-netG = generator.Generator(args.input_nc, args.output_nc)
+# netG = generator.Generator(args.input_nc, args.output_nc)
+netG = generator.Generator(3, num_filter = 32, output_dim=args.output_nc, num_resnet=6)
 netD = discriminator.Discriminator(args.input_nc)
 
 if device.type == 'cuda':
@@ -49,8 +53,8 @@ identityLoss = nn.L1Loss().to(device)
 G_optimizer = optim.Adam(netG.parameters())
 D_optimizer = optim.Adam(netD.parameters())
 
-target_real = Variable(Tensor(batch_size).fill_(1.0), requires_grad=False)
-target_fake = Variable(Tensor(batch_size).fill_(0.0), requires_grad=False)
+target_real = Variable(Tensor(batch_size).fill_(1.0), requires_grad=False).to(device)
+target_fake = Variable(Tensor(batch_size).fill_(0.0), requires_grad=False).to(device)
 
 print("All set, Training Begins!")
 
@@ -63,7 +67,7 @@ transforms_ = [transforms.Resize(256, Image.BILINEAR),
 dataloader = DataLoader(ImageDataset(data_path, transforms_ = transforms_, unaligned=True), batch_size = batch_size, shuffle=True, num_workers=0)
 
 
-for epoch in range(n_epochs):
+for epoch in trange(n_epochs, leave=False):
     G_loss = []
     D_loss = []
     recLoss1 = []
@@ -84,6 +88,9 @@ for epoch in range(n_epochs):
 
         realA = realA.to(device)
         realB = realB.to(device)
+
+        hashA = hashA.to(device)
+        hashB = hashB.to(device)
 
         conditionedA = conditionedA.to(device)
         conditionedB = conditionedB.to(device)
@@ -111,10 +118,14 @@ for epoch in range(n_epochs):
         loss_B2A = ganLoss(pred_fakeA, target_real)
 
         ## Reconstruction loss
-        recoveredA = netG(fakeB)
+
+        fakeConditionedA = torch.cat((fakeA, hashA, hashB), 1).to(device)
+        fakeConditionedB = torch.cat((fakeB, hashB, hashA), 1).to(device)
+
+        recoveredA = netG(fakeConditionedB)
         cycle_lossA = reconstructionLoss(realA, recoveredA)
 
-        recoveredB = netG(fakeA)
+        recoveredB = netG(fakeConditionedA)
         cycle_lossB = reconstructionLoss(realB, recoveredB)
 
         ## Total loss
@@ -135,7 +146,7 @@ for epoch in range(n_epochs):
         lossD_fake1 = ganLoss(pred_fake1, target_fake)
 
         pred_real2 = netD(realB)
-        lossD_real1 = ganLoss(pred_real2, target_real)
+        lossD_real2 = ganLoss(pred_real2, target_real)
 
         pred_fake2 = netD(fakeB.detach())
         lossD_fake2 = ganLoss(pred_fake2, target_fake)
@@ -145,3 +156,7 @@ for epoch in range(n_epochs):
 
         loss_D_1.backward()
         D_optimizer.step()
+
+print("Finished Training!")
+torch.save(netG.state_dict(), model_dir + 'common generator.pkl')
+torch.save(netD.state_dict(), model_dir + 'common Discriminator.pkl')
