8e69e1db97fa8c5b1818615bd63409eba060d1bc
Bhadrinath Nagabandi (Student) <bnagaban@asu.edu>
2019-04-17 23:21:21 -0700

    fixed errors and added conidtioning

--- train.py
+++ train.py
@@ -9,22 +9,28 @@ import torch
 import torch.optim as optim
 import generator
 import discriminator
+import torch.nn as nn
+from torch import Tensor
 from datasets import ImageDataset
+import utils
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--epoch', type=int, default=0, help='starting epoch')
 parser.add_argument('--n_epochs', type=int, default=200, help='number of epochs of training')
-parser.add_argument('--batchSize', type=int, default=16, help='size of the batches')
+parser.add_argument('--batchSize', type=int, default=64, help='size of the batches')
 parser.add_argument('--dataroot', type=str, default='datasets/horse2zebra/', help='root directory of the dataset')
 parser.add_argument('--lr', type=float, default=0.0002, help='initial learning rate')
 parser.add_argument('--decay_epoch', type=int, default=100, help='epoch to start linearly decaying the learning rate to 0')
 parser.add_argument('--size', type=int, default=256, help='size of the data crop (squared assumed)')
-parser.add_argument('--input_nc', type=int, default=3, help='number of channels of input data')
+parser.add_argument('--input_nc', type=int, default=5, help='number of channels of input data')
 parser.add_argument('--output_nc', type=int, default=3, help='number of channels of output data')
 args = parser.parse_args()
 print(args)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+data_path = "./datasets/facades/"
+batch_size = args.batchSize
+n_epochs = args.n_epochs
 
 if torch.cuda.is_available():
     print("GPU is available!")
@@ -48,7 +54,16 @@ target_fake = Variable(Tensor(batch_size).fill_(0.0), requires_grad=False)
 
 print("All set, Training Begins!")
 
-for epoch in n_epochs:
+transforms_ = [transforms.Resize(256, Image.BILINEAR),
+               transforms.RandomCrop(224),
+               transforms.RandomHorizontalFlip(),
+               transforms.ToTensor(),
+               transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))]
+
+dataloader = DataLoader(ImageDataset(data_path, transforms_ = transforms_, unaligned=True), batch_size = batch_size, shuffle=True, num_workers=0)
+
+
+for epoch in range(n_epochs):
     G_loss = []
     D_loss = []
     recLoss1 = []
@@ -57,64 +72,76 @@ for epoch in n_epochs:
     ############################### LOAD DATA BELOW ##################
     # conditioned text at the end of every batch
     # move data to cuda
-    for (realA, labelA), (realB, labelB) in zip(trainloaderA, trainloaderB):
+    for i, batch in enumerate(dataloader):
+        realA = batch['A']
+        realB = batch['B']
+
+        hashB = utils.hash('B', realB, batch_size)
+        hashA = utils.hash('A', realA, batch_size)
+
+        conditionedA = torch.cat((realA, hashA, hashB), 1)
+        conditionedB = torch.cat((realB, hashB, hashA), 1)
+
+        realA = realA.to(device)
+        realB = realB.to(device)
+
+        conditionedA = conditionedA.to(device)
+        conditionedB = conditionedB.to(device)
+
+        ###############################
+        G_optimizer.zero_grad()
+
+        ## from a domain to same domain
+        ## source condtion and target condition should be the same
+
+        sameA = netG(conditionedA)
+        identityA = identityLoss(sameA, realA)
+
+        sameB = netG(conditionedB)
+        identityB = identityLoss(sameB, realB)
+
+        ## GAN Loss
+        ## condition the img from source A to target B
+        fakeB = netG(conditionedA)
+        pred_fakeB = netD(fakeB)
+        loss_A2B = ganLoss(pred_fakeB, target_real)
+
+        fakeA = netG(conditionedB)
+        pred_fakeA = netD(fakeA)
+        loss_B2A = ganLoss(pred_fakeA, target_real)
+
+        ## Reconstruction loss
+        recoveredA = netG(fakeB)
+        cycle_lossA = reconstructionLoss(realA, recoveredA)
+
+        recoveredB = netG(fakeA)
+        cycle_lossB = reconstructionLoss(realB, recoveredB)
+
+        ## Total loss
+        G_loss = loss_A2B + loss_B2A + cycle_lossA + cycle_lossB
+
+        G_loss.backward()
+        G_optimizer.step()
 
 
+        ######################################
+        D_optimizer.zero_grad()
 
-    ###############################
-    G_optimizer.zero_grad()
+        ## from D-A
+        pred_real1 = netD(realA)
+        lossD_real1 = ganLoss(pred_real1, target_real)
 
-    ## from a domain to same domain
-    ## source condtion and target condition should be the same
+        pred_fake1 = netD(fakeA.detach())
+        lossD_fake1 = ganLoss(pred_fake1, target_fake)
 
-    sameA = netG(realA)
-    identityA = identityLoss(sameA, realA)
+        pred_real2 = netD(realB)
+        lossD_real1 = ganLoss(pred_real2, target_real)
 
-    sameB = netG(realB)
-    identityB = identityLoss(sameB, realB)
-
-    ## GAN Loss
-    ## condition the img from source A to target B
-    fakeB = netG(realA)
-    pred_fakeB = netD(fakeB)
-    loss_A2B = ganLoss(pred_fakeB, target_real)
-
-    fakeA = netG(realB)
-    pred_fakeA = netD(fakeA)
-    loss_B2A = ganLoss(pred_fakeA, target_real)
-
-    ## Reconstruction loss
-    recoveredA = netG(fakeB)
-    cycle_lossA = reconstructionLoss(realA, recoveredA)
-
-    recoveredB = netG(fakeA)
-    cycle_lossB = reconstructionLoss(realB, recoveredB)
-
-    ## Total loss
-    G_loss = loss_A2B + loss_B2A + cycle_lossA + cycle_lossB
-
-    G_loss.backward()
-    G_optimizer.step()
+        pred_fake2 = netD(fakeB.detach())
+        lossD_fake2 = ganLoss(pred_fake2, target_fake)
 
 
-    ######################################
-    D_optimizer.zero_grad()
+        loss_D_1 = lossD_real1 + lossD_fake1 + lossD_real2 + lossD_fake2
 
-    ## from D-A
-    pred_real1 = netD(realA)
-    lossD_real1 =
-
-    pred_fake1 = netD(fakeA.detach())
-    lossD_fake1 =
-
-    pred_real2 = netD(realB)
-    lossD_real1 =
-
-    pred_fake2 = netD(fakeB.detach())
-    lossD_fake2 =
-
-
-    loss_D_1 = lossD_real1 + lossD_fake1 + lossD_real2 + lossD_fake2
-
-    loss_D_1.backward()
-    D_optimizer.step()
+        loss_D_1.backward()
+        D_optimizer.step()
